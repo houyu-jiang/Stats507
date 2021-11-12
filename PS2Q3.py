050a2d4804a1e29b340bd16de7bc61dad0e0fb31
apple <apple@appledeMacBook-Pro-12.local>
2021-11-12 00:01:04 -0500

    updated PS2Q3.py

--- PS2Q3.py
+++ PS2Q3.py
@@ -1,125 +1,201 @@
+import numpy as np
+import pandas as pd
+from os.path import exists
+from math import floor 
+from timeit import Timer
+from collections import defaultdict
+from IPython.core.display import display, HTML
+
 # ## Question 3
+# In this question we clean an append demographic and dentition data from
+# several cohorts of the NHANES data. 
 
-# #### a)
+# file location: -------------------------------------------------------------
+path = './'
 
-# +
-url1 = "https://wwwn.cdc.gov/Nchs/Nhanes/2011-2012/DEMO_G.XPT"
-url2 = "https://wwwn.cdc.gov/Nchs/Nhanes/2013-2014/DEMO_H.XPT"
-url3 = "https://wwwn.cdc.gov/Nchs/Nhanes/2015-2016/DEMO_I.XPT"
-url4 = "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/DEMO_J.XPT"
+# column maps: ---------------------------------------------------------------
+# new names for demo cols
+demo_cols = {
+    'SEQN': 'id',
+    'RIDAGEYR': 'age',
+    'RIAGENDR': 'gender',
+    'RIDRETH3': 'race',
+    'DMDEDUC2': 'education',
+    'DMDMARTL': 'marital_status',
+    'RIDSTATR': 'exam_status',
+    'SDMVPSU': 'psu',
+    'SDMVSTRA': 'strata',
+    'WTMEC2YR': 'exam_wt',
+    'WTINT2YR': 'interview_wt'
+    }
 
-NHANES_1112 = pd.read_sas(url1)
-NHANES_1314 = pd.read_sas(url2)
-NHANES_1516 = pd.read_sas(url3)
-NHANES_1718 = pd.read_sas(url4)
+# new names for ohx cols
+ohx_cols = {'SEQN': 'id', 'OHDDESTS': 'dentition_status'}
+tc_cols = {'OHX' + str(i).zfill(2) + 'TC':
+           'tc_' + str(i).zfill(2) for i in range(1, 33)}
+ctc_cols = {'OHX' + str(i).zfill(2) + 'CTC':
+            'ctc_' + str(i).zfill(2) for i in range(2, 32)}
+_, _ = ctc_cols.pop('OHX16CTC'), ctc_cols.pop('OHX17CTC')
 
-NHANES_1112 = NHANES_1112.drop_duplicates(subset='SEQN')
-NHANES_1314 = NHANES_1314.drop_duplicates(subset='SEQN')
-NHANES_1516 = NHANES_1516.drop_duplicates(subset='SEQN')
-NHANES_1718 = NHANES_1718.drop_duplicates(subset='SEQN')
+ohx_cols.update(tc_cols)
+ohx_cols.update(ctc_cols)
 
-NHANES_1112["cohort"] = "11-12"
-NHANES_1314["cohort"] = "13-14"
-NHANES_1516["cohort"] = "15-16"
-NHANES_1718["cohort"] = "17-18"
+# columns to convert to integer
+demo_int = ('id', 'age', 'psu', 'strata')
+ohx_int = ('id', )
 
-NHANES_all = NHANES_1112.append(NHANES_1314, ignore_index = True)
-NHANES_all = NHANES_all.append(NHANES_1516, ignore_index = True)
-NHANES_all = NHANES_all.append(NHANES_1718, ignore_index = True)
-# -
+# levels for categorical variables
+demo_cat = {
+    'gender': {1: 'Male', 2: 'Female'},
+    'race': {1: 'Mexican American',
+             2: 'Other Hispanic',
+             3: 'Non-Hispanic White',
+             4: 'Non-Hispanic Black',
+             6: 'Non-Hispanic Asian',
+             7: 'Other/Multiracial'
+             },
+    'education': {1: 'Less than 9th grade',
+                  2: '9-11th grade (Includes 12th grade with no diploma)',
+                  3: 'High school graduate/GED or equivalent',
+                  4: 'Some college or AA degree',
+                  5: 'College graduate or above',
+                  7: 'Refused',
+                  9: "Don't know"
+                  },
+    'marital_status': {1: 'Married',
+                       2: 'Widowed',
+                       3: 'Divorced',
+                       4: 'Separated',
+                       5: 'Never married',
+                       6: 'Living with partner',
+                       77: 'Refused',
+                       99: "Don't know"
+                       },
+    'exam_status': {1: 'Interviewed only',
+                    2: 'Both interviewed and MEC examined'
+                    }
+    }
 
-NHANES_all = NHANES_all[["cohort", "SEQN", "RIDAGEYR",
-                         "RIDRETH3", "DMDEDUC2", "DMDMARTL",
-                         "RIDSTATR", "SDMVPSU", "SDMVSTRA",
-                         "WTMEC2YR", "WTINT2YR"]]
+ohx_cat = {
+    'dentition_status': {1: 'Complete', 2: 'Partial', 3: 'Not Done'}
+    }
 
-NHANES_all = NHANES_all.astype({"SEQN": pd.Int64Dtype(),
-                                "RIDAGEYR": pd.CategoricalDtype(),
-                                "RIDRETH3": pd.CategoricalDtype(),
-                                "DMDEDUC2": pd.CategoricalDtype(),
-                                "DMDMARTL": pd.CategoricalDtype(),
-                                "RIDSTATR": pd.CategoricalDtype(),
-                                "SDMVPSU": pd.CategoricalDtype(),
-                                "SDMVSTRA": pd.CategoricalDtype()}) 
-variable_list = ["SEQN", "RIDAGEYR", "RIDRETH3", "DMDEDUC2", "DMDMARTL",
-                 "RIDSTATR", "SDMVPSU", "SDMVSTRA", "WTMEC2YR", "WTINT2YR"]
-rename_list = ["unique_id", "age", "race_and_ethnicity", "education",
-               "marital_status","intervew_and_exam_status", "pseudo-PSU",
-               "pseudo-stratum","MEC_exam_weight", "interview weight"]
-dic = dict(zip(variable_list, rename_list))
-NHANES_all.rename(columns=dic)
+tc = {
+      1: 'Primary tooth present',
+      2: 'Permanent tooth present',
+      3: 'Dental Implant',
+      4: 'Tooth not present',
+      5: 'Permanent dental root fragment present',
+      9: 'Could not assess'
+      }
 
-NHANES_all.to_pickle("demographic.pickle")
+ctc = (
+ {
+  'A': 'Primary tooth with a restored surface condition',
+  'D': 'Sound primary tooth',
+  'E': 'Missing due to dental disease',
+  'F': 'Permanent tooth with a restored surface condition',
+  'J':
+    'Permanent root tip is present but no restorative replacement is present',
+  'K': 'Primary tooth with a dental carious surface condition',
+  'M': 'Missing due to other causes',
+  'P':
+    'Missing due to dental disease but replaced by a removable restoration',
+  'Q':
+    'Missing due to other causes but replaced by a removable restoration',
+  'R':
+    'Missing due to dental disease but replaced by a fixed restoration',
+  'S': 'Sound permanent tooth',
+  'T':
+    'Permanent root tip is present but a restorative replacement is present',
+  'U': 'Unerupted',
+  'X': 'Missing due to other causes but replaced by a fixed restoration',
+  'Y': 'Tooth present, condition cannot be assessed',
+  'Z': 'Permanent tooth with a dental carious surface condition'
+ })
 
-# #### b)
+# read data: -----------------------------------------------------------------
+base_url = 'https://wwwn.cdc.gov/Nchs/Nhanes/'
+cohorts = (
+    ('2011-2012', 'G'),
+    ('2013-2014', 'H'),
+    ('2015-2016', 'I'),
+    ('2017-2018', 'J')
+    )
+# demographic data
+demo_file = path + '/demo.feather'
 
-# +
-url5 = "https://wwwn.cdc.gov/Nchs/Nhanes/2011-2012/OHXDEN_G.XPT"
-url6 = "https://wwwn.cdc.gov/Nchs/Nhanes/2013-2014/OHXDEN_H.XPT"
-url7 = "https://wwwn.cdc.gov/Nchs/Nhanes/2015-2016/OHXDEN_I.XPT"
-url8 = "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/OHXDEN_J.XPT"
+if exists(demo_file):
+    demo = pd.read_feather(demo_file)
+else:
+    demo_cohorts = {}
+    for cohort, label in cohorts:
 
-OHD_1112 = pd.read_sas(url5)
-OHD_1314 = pd.read_sas(url6)
-OHD_1516 = pd.read_sas(url7)
-OHD_1718 = pd.read_sas(url8)
+        # read data and subset columns
+        url = base_url + cohort + '/DEMO_' + label + '.XPT'
+        dat = pd.read_sas(url).copy()
+        dat = dat[list(demo_cols.keys())].rename(columns=demo_cols)
 
-OHD_1112 = OHD_1112.drop_duplicates(subset='SEQN')
-OHD_1314 = OHD_1314.drop_duplicates(subset='SEQN')
-OHD_1516 = OHD_1516.drop_duplicates(subset='SEQN')
-OHD_1718 = OHD_1718.drop_duplicates(subset='SEQN')
+        # assign cohort and collect
+        dat['cohort'] = cohort
+        demo_cohorts.update({cohort: dat})
 
-OHD_1112["cohort"] = "11-12"
-OHD_1314["cohort"] = "13-14"
-OHD_1516["cohort"] = "15-16"
-OHD_1718["cohort"] = "17-18"
+    # concatenate and save
+    demo = pd.concat(demo_cohorts, ignore_index=True)
+ 
+    # categorical variables
+    for col, d in demo_cat.items():
+        demo[col] = pd.Categorical(demo[col].replace(d))
+    demo['cohort'] = pd.Categorical(demo['cohort'])
 
-OHD_all = OHD_1112.append(OHD_1314, ignore_index = True)
-OHD_all = OHD_all.append(OHD_1516, ignore_index = True)
-OHD_all = OHD_all.append(OHD_1718, ignore_index = True)
-# -
+    # integer variables
+    for col in demo_int:
+        demo[col] = pd.to_numeric(demo[col], downcast='integer')
 
-# Select targeted columns
-lst = OHD_all.columns
-variable_list_OHD = ["cohort", "SEQN", "OHDDESTS"]
-temp_list = []
-for item in lst:
-    if re.match(r'OHX\d+\d+TC', item) or re.match(r'OHX\d+\d+CTC', item):
-        variable_list_OHD.append(item)
-        temp_list.append(item)
-OHD_all = OHD_all[variable_list_OHD]
+    demo.to_feather(demo_file)
+demo.shape
 
-# Change Category
-dic2 = {}
-for item in variable_list_OHD:
-    if item == "cohort":
-        dic2[item] = pd.StringDtype()
-    elif item == "SEQN":
-        dic2[item] = pd.Int64Dtype()
-    else:
-        dic2[item] = pd.CategoricalDtype()
-OHD_all = OHD_all.astype(dic2) 
+# dentition data
+ohx_file = path + '/ohx.feather'
 
-# Rename
-rename_list2 = []
-for item in temp_list:
-    if len(item) == 7:
-        rename_list2.append("tooth count: " + item[3:5])
-    else:
-        rename_list2.append("coronal cavities tooth count: " + item[3:5])
-dic3 = dict(zip(temp_list, rename_list2))
-dic3["SEQN"] = "unique id"
-dic3["OHDDESTS"] = "overall oral health status"
-OHD_all.rename(columns=dic3)
+if exists(ohx_file):
+    ohx = pd.read_feather(ohx_file)
+else:
+    ohx_cohorts = {}
+    for cohort, label in cohorts:
 
-OHD_all.to_pickle("oral_health.pickle")
+        # read data and subset columns
+        url = base_url + cohort + '/OHXDEN_' + label + '.XPT'
+        dat = pd.read_sas(url).copy()
+        dat = dat[list(ohx_cols.keys())].rename(columns=ohx_cols)
 
-# #### c)
+        # assign cohort and collect
+        dat['cohort'] = cohort
+        ohx_cohorts.update({cohort: dat})
+ 
+    # concatenate
+    ohx = pd.concat(ohx_cohorts, ignore_index=True)
 
-# The number of case in the demographic datasets is:
+    # categorical variables
+    for col, d in ohx_cat.items():
+        ohx[col] = pd.Categorical(ohx[col].replace(d))
+    
+    for col in tc_cols.values():
+        ohx[col] = pd.Categorical(ohx[col].replace(tc))
 
-NHANES_all.shape[0]
+    # ctc columns get read in as bytes
+    for col in ctc_cols.values():
+        ohx[col] = ohx[col].apply(lambda x: x.decode('utf-8'))
+        ohx[col] = pd.Categorical(ohx[col].replace(ctc))
 
-# The number of case in the oral health and dentition data is:
+    ohx['cohort'] = pd.Categorical(ohx['cohort'])
+    # integer variables
+    for col in ohx_int:
+        ohx[col] = pd.to_numeric(ohx[col], downcast='integer')
 
-OHD_all.shape[0]
+    # save
+    ohx.to_feather(ohx_file)
+ohx.shape
+# ---
+
+# The demographic and dentition datasets just created have 39,156 and 35,909 cases, respectively.
